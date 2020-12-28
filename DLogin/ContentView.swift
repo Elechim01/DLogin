0df2518dcf70ccad9f1b1f961365877de1f74a6c
Elechim01 <mikymanny015@gmail.com>
2020-12-28 18:06:41 +0100

    Initial Commit

---
+++ DLogin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DLogin
+//
+//  Created by Michele on 28/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DLogin/DLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  DLoginApp.swift
+//  DLogin
+//
+//  Created by Michele on 28/12/20.
+//
+
+import SwiftUI
+
+@main
+struct DLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
